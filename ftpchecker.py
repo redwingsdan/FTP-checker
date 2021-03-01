eeef77bf414501bd42c7e11ff3d0758310f8d819
Daniel <djp21795@yahoo.com>
2021-02-28 20:50:22 -0500

    Added config

--- ftpchecker.py
+++ ftpchecker.py
@@ -2,44 +2,56 @@ import yagmail
 import ftplib
 import schedule
 import time
+import configparser
 from ftplib import FTP_TLS
 
 def send_email():
-    USER = 'gmailacct@gmail.com'
-    PASS = 'gmailacctpassword'
-    TO_EMAILS = ['toaddress@address.com', 'othertoaddress@secondaddress.com']
-    SUBJECT = 'Email Subject'
-    EMAIL_CONTENT = 'This is the content of the email'
+    global configParser
+    USER = configParser.get('email-config', 'username')
+    PASS = configParser.get('email-config', 'password')
+    TO_EMAILS = [e.strip() for e in configParser.get('email-config', 'to_emails').split(',')]
+    SUBJECT = 'FTP File'
+    EMAIL_CONTENT = 'There was a file added to the FTP server'
     try:
         #initializing the server connection
-        yag = yagmail.SMTP(user=USER, password=PASS')
+        yag = yagmail.SMTP(user=USER, password=PASS)
         #sending the email
         yag.send(to=TO_EMAILS, subject=SUBJECT, contents=EMAIL_CONTENT)
         print("Email sent successfully")
-    except:
-        print("Error, email was not sent")       
+    except all_errors as e:
+        print("Error, email was not sent", e)       
         
 def check_ftp_for_file(t):
-    FTP_SERVER = 'ftp.pureftpd.org'
-    USER = 'ftp user'
-    PASS = 'ftp pass'
-    DIR = 'file/dir/path'
-    FILE_NAME = 'file.txt'
+    global configParser
+    global prev_file_size
+    FTP_SERVER = configParser.get('ftp-config', 'servername')
+    USER = configParser.get('ftp-config', 'username')
+    PASS = configParser.get('ftp-config', 'password')
+    DIR = configParser.get('ftp-config', 'directory')
+    FILE_NAME = configParser.get('ftp-config', 'filename')
     ftps = FTP_TLS(FTP_SERVER)
     try:
         ftps.login(USER, PASS)
         ftps.prot_p()  # switch to secure data connection. Otherwise, only the user and password is encrypted and not all the file data.
         ftps.cwd(DIR)
         size = ftps.size(FILE_NAME)
-        send_email()
+        if prev_file_size != size:
+            prev_file_size = size
+            send_email()
     except ftplib.all_errors as e:
+        prev_file_size = 0
         print('FTP error:', e)
     finally:
         ftps.close()
 
-schedule.every().minute.do(check_ftp_for_file, 'Checking FTP')        
+configParser = configparser.RawConfigParser()   
+configFilePath = r'ftpconfig.txt'
+configParser.read(configFilePath)
+scheduler_minutes = int(configParser.get('ftp-config', 'scheduler_minutes'))
+schedule.every(scheduler_minutes).minutes.do(check_ftp_for_file, 'Checking FTP')       
+prev_file_size = 0 
 while True:
     schedule.run_pending()
     print('Waiting to check ftp')
-    time.sleep(60) # wait one minute
+    time.sleep(scheduler_minutes * 60) # wait one minute
 #check_ftp_for_file(None)
